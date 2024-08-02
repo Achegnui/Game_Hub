24e2069d1c44304fcf8dc51c39d450c9e6712c59
Achegnui Juven <achegnuijuven@gmail.com>
2024-08-02 19:54:24 +0100

    implementGenreListItem by fetching

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genres {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genres[];
+}
+
+const useGenres = () => {
+  const controller = new AbortController();
+  const [genres, setgames] = useState<Genres[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    apiClient
+      .get<FetchGenresResponse>("/games", { signal: controller.signal })
+      .then((respond) => {
+        setgames(respond.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+export default useGenres;
